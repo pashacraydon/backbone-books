0a1dca79c10ff88e213caeb9b511ae7ac8023620
Pashasc <pasha@safaribooksonline.com>
2013-05-29 16:56:21 -0700

    added shim dependencies

--- js/app.js
+++ js/app.js
@@ -6,10 +6,23 @@ requirejs.config({
     "baseUrl": "js/libs",
     "paths": {
       "app": "../app",
-      "jquery": "//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min",
+      "jquery": "//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.1/jquery.min",
       "jqueryui": "//ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/jquery-ui.min",
+      "modernizr": "//cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min",
       "underscore": "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore-min",
       "backbone": "//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min"
+    },
+    shim: {
+    	'backbone': {
+    		deps: ['underscore', 'jquery'],
+    		exposts: 'backbone'
+    	},
+    	'underscore': {
+    		exports: '_'
+    	},
+    	'booklet': {
+    		deps: ['jquery', 'backbone', 'jqueryui', 'moderniz']
+    	}
     }
 });
 
