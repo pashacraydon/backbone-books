c0d84460131c86ffaead91a79db5c978b6d60ace
PashaTNT <pasha@pashasolomon.com>
2013-11-04 19:55:24 -0800

    fixes

--- js/app/utils/variables.js
+++ js/app/utils/variables.js
@@ -5,7 +5,6 @@ define(function (require) {
   data = {
     //Please use you're own API key, thanks!
     API_KEY: 'AIzaSyBhBph_ccmIlFn9YSrvhCE_8zrYxazyqJ8',
-    //API_KEY: 'AIzaSyAUpierWu7ydjKsa2141jS55CCnqu7JXZo',
     MAX_DEFAULT: 12,
     TOPICS: [
     	{ subject:"Arts" },
